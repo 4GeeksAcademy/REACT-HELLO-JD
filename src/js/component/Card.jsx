c414dbe7bfa231b340ddfbff9004e627314d4c58
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-05-28 10:41:20 +0000

    terminando las cards

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -6,13 +6,15 @@ const Card = ({image, title, text ,buttonName}) => {
   return (
 
     
-      <div className="card my-3 " style={{width: "20 rem"}}>
-         {/* <img src={image} className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{text}</p>
-            <a href="#" className="btn btn-primary">{buttonName}</a>
-          </div> */}
+<div className="card my-3 text-center" style={{ width: "18rem", display: "inline-block", margin: "10px" }}>
+      <img src={image} className="card-img-top" alt="..." style={{ height: "290px", objectFit: "cover" }}/>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <div className="div">
+        <a href="#" className="btn btn-primary mt-auto align-self-center">{buttonName}</a>
+        </div>
+          </div>
         </div>
 
   )
@@ -22,7 +24,7 @@ Card.proptypes = {
 image: Proptypes.string.isRequired,
 title: Proptypes.string.isRequired,
 text: Proptypes.string.isRequired,
-
+buttonName: Proptypes.string.isRequired,
 }
 
 export default Card;
--- src/js/component/ListCard.jsx
+++ src/js/component/ListCard.jsx
@@ -7,19 +7,18 @@ import Proptypes from 'prop-types';
 const ListCard = () => {
 
     const cardsData = [
-        { title: "Titulo1", image: "", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, harum voluptate ad fuga eveniet error." },
-        { title: "Titulo2", image: "", text: "Bacon ipsum dolor amet kielbasa ground round salami cow. Bresaola jerky pork chop tongue porchetta." },
-        { title: "Titulo3", image: "", text: "Ball tip turkey tenderloin tail pork loin, pastrami buffalo boudin ham hock. Corned beef turkey." },
-        { title: "Titulo4", image: "", text: "Cow tenderloin tongue cupim jerky beef ribs chicken buffalo pig jowl kielbasa. Alcatra cow brisket." },
+        { id:1, title: "Ruta de los Acantilados de Benitachell", image: "https://cristalderoca.com/senderismo/wp-content/uploads/2022/06/senderismo-cristal-de-roca-benitachell-acantilados-cueva-1-768x512.jpg", text: "Los sorprendentes acantilados de Benitatxell que vamos a visitar se extienden entre las calas del Moraig y del Llebeig, con más de 1,5 kilómetros de paredes rocosas cuya cúspide supera, en varios puntos, los 150 metros de altitud sobre el nivel del mar." },
+        { id:2,title: "Ruta de Moraira al Cap D´Or", image: "https://cristalderoca.com/senderismo/wp-content/uploads/2022/06/senderismo-cristal-de-roca-benitachell-acantilados-destacada-1.jpg", text: "El Cap d’Or es también conocido como Punta de Moraira y se encuentra al este del pequeño núcleo urbano de Moraira, el cual forma municipio junto a Teulada. Esta pequeña lengua rocosa no supera el kilómetro de longitud, y alcanza los 166 metros de altitud sobre el mar Mediterráneo, con la Torre del Cap d’Or coronando la gran roca." },
+        { id:3,title: "Visita a la Cueva de las Calaveras", image: "https://cristalderoca.com/senderismo/wp-content/uploads/2022/06/senderismo-cristal-de-roca-benitachell-cueva-calaveras-2.jpg", text: "Ball tip turkey tenderloin tail pork loin, pastrami buffalo boudin ham hock. Corned beef turkey." },
+        { id:4,title: "El Parque Natural de La Serra Gelada ", image: "https://cristalderoca.com/senderismo/wp-content/uploads/2022/01/senderismo-cristal-de-roca-faro-de-albir.jpg", text: "Entre las Bahías de Benidorm y Alfaz del Pi, el parque natural de la Sierra Gelada abarca 6 kilómetros de litoral, dando lugar a unos impresionantes acantilados y una de las dunas fósiles mas altas e Europa. En el extremo norte del parque natural, en la Punta Bombarda, se ubica el Faro del Albir, que desde 1893 es fiel referencia para los navegantes. Un agradable paseo donde disfutaremos de una preciosas vistas de la Bahía de Altea, la Sierra de Bernia y la propia Serra Gelada." },
     ];
-
-
+const buttonName= "Rutas turisticas"
     return (
-        <div className="container">
+        <div className="container-fluid d-inline-flex">
             <div className="row justify-content-center g-3 mt-2 mb-2">
-                {/* {cardsData.map((card) => {
-                    return <Card title={card.title} text={card.text} image={card.image} />
-                })} */}
+                {cardsData.map((card) => {
+                    return <Card title={card.title} key={card.id} text={card.text} image={card.image} buttonName={buttonName}/>
+                })}
 
             </div>
         </div>
